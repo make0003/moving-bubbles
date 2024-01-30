1b7009413fa14c3b77bc0fe8b6208d2333ee314a
Mathilde <make0003@stud.kea.dk>
2024-01-30 14:40:49 +0100

    1000

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ setInterval(() => {
     bubble.style.transform = `translate(${Math.random() * 100}vw, ${Math.random() * 100}vh)`;
     bubble.style.background = `hsl(${Math.random() * 360}, 70%, 70%)`;
   });
-}, 2000);
+}, 1000);
